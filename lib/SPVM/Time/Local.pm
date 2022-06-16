29ad1fe1bff75ac13e530f664b9e93dfaa5cc41e
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-16 12:24:54 +0900

    First commit

---
+++ lib/SPVM/Time/Local.pm
@@ -0,0 +1,49 @@
+package SPVM::Time::Local;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 NAME
+
+SPVM::Time::Local - Time::Local is a SPVM module
+
+=head1 SYNOPSYS
+
+  use Time::Local;
+  
+=head1 DESCRIPTION
+
+Time::Local is a SPVM module.
+
+=head1 STATIC METHODS
+
+
+
+=head1 INSTANCE METHODS
+
+
+
+=head1 REPOSITORY
+
+
+
+=head1 BUG REPORT
+
+
+
+=head1 SUPPORT
+
+
+
+=head1 AUTHOR
+
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright YYYY-YYYY AUTHOR_NAME, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
