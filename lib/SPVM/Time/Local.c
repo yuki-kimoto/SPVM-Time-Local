a7e5a9b48c93459a7934d366e4326dade603be72
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-16 12:41:52 +0900

    Implement Time::Local

--- lib/SPVM/Time/Local.c
+++ lib/SPVM/Time/Local.c
@@ -1,12 +1,79 @@
+#define _XOPEN_SOURCE
+
 #include "spvm_native.h"
 
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <locale.h>
 
+static const char* FILE_NAME = "SPVM/Time/Local.c";
 
-int32_t SPVM__Time__Local__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Time__Local__time(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
   (void)stack;
+  
+  int64_t epoch = (int64_t)time(NULL);
+  
+  stack[0].lval = epoch;
   
   return 0;
 }
 
+int32_t SPVM__Time__Local__localtime(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t e;
+  
+  time_t time = (time_t)stack[0].lval;
+  struct tm* st_tm = env->alloc_memory_block_zero(env, sizeof(struct tm));
+  
+#ifdef _WIN32
+  localtime_s(st_tm, &time);
+#else
+  localtime_r(&time, st_tm);
+#endif
+  
+  void* obj_time_info = env->new_pointer_by_name(env, "Time::Info", st_tm, &e, FILE_NAME, __LINE__);
+  if (e) { return e; }
+  
+  stack[0].oval = obj_time_info;
+  
+  return 0;
+}
 
+int32_t SPVM__Time__Local__gmtime(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t e;
+  
+  time_t time = (time_t)stack[0].lval;
+  struct tm* st_tm = env->alloc_memory_block_zero(env, sizeof(struct tm));
+  
+#ifdef _WIN32
+  gmtime_s(st_tm, &time);
+#else
+  gmtime_r(&time, st_tm);
+#endif
+  
+  void* obj_time_info = env->new_pointer_by_name(env, "Time::Info", st_tm, &e, FILE_NAME, __LINE__);
+  if (e) { return e; }
+  
+  stack[0].oval = obj_time_info;
+  
+  return 0;
+}
+
+int32_t SPVM__Time__Local__timelocal(SPVM_ENV* env, SPVM_VALUE* stack) {
+  int32_t e;
+  
+  void* obj_time_info = stack[0].oval;
+  if (!obj_time_info) { return env->die(env, "Time::Info object must be defined", FILE_NAME, __LINE__); }
+  
+  struct tm* st_tm = env->get_pointer(env, obj_time_info);
+
+  int64_t ltime = (int64_t)mktime(st_tm);
+  
+  stack[0].lval = ltime;
+  
+  return 0;
+}
--- lib/SPVM/Time/Local.pm
+++ lib/SPVM/Time/Local.pm
@@ -1,49 +1,43 @@
 package SPVM::Time::Local;
 
-our $VERSION = '0.01';
-
 1;
 
 =head1 NAME
 
-SPVM::Time::Local - Time::Local is a SPVM module
+SPVM::Time::Local - Time::Local manipulation
 
 =head1 SYNOPSYS
-
+  
   use Time::Local;
   
+  # Convert Time::Info object that is local time to epoch
+  my $time = Time::Local->timelocal($time_info);
+  
+  # Convert Time::Info object that is UTC to epoch
+  my $time = Time::Local->timegm($time_info);
+
 =head1 DESCRIPTION
 
-Time::Local is a SPVM module.
+Time::Local  - Reverse Manipulation of localtime and gmtime functions.
 
-=head1 STATIC METHODS
+=head1 CLASS METHODS
 
+=head2 timelocal
 
+  static method timelocal : long ($time_info : Time::Info)
 
-=head1 INSTANCE METHODS
+Convert L<Time::Info|SPVM::Time::Info> object that is local time to epoch.
 
+C<wday> and C<yday> is ignored.
 
+  my $time = Time::Local->timelocal($time_info);
 
-=head1 REPOSITORY
+=head2 timegm
 
+  static method timegm : long ($time_info : Time::Info)
 
+Convert L<Time::Info|SPVM::Time::Info> object that is UTC to epoch.
 
-=head1 BUG REPORT
+C<wday> and C<yday> is ignored.
 
-
-
-=head1 SUPPORT
-
-
-
-=head1 AUTHOR
-
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright YYYY-YYYY AUTHOR_NAME, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
+  my $time = Time::Local->timegm($time_info);
